6f78c46b820c2f12c2c99e4275955f925bc81a04
Marius Storhaug <marstor@hotmail.com>
2024-11-03 10:15:20 +0100

    Fix the secret test

--- tests/Store.Tests.ps1
+++ tests/Store.Tests.ps1
@@ -51,7 +51,13 @@
             $something | Should -Be 'Something'
         }
         It 'Should be able to set a secret' {
-            Set-StoreConfig -Name 'Secret' -Value ('Something' | ConvertTo-SecureString -AsPlainText -Force)
+            # $Secure = Read-Host -AsSecureString # Enter 'Something'
+            # $secret = ConvertFrom-SecureString -SecureString $Secure
+            $secret = '01000000d08c9ddf0115d1118c7a00c04fc297eb01000000d8f5081752e7fe41b9e40cd0f11a075a0000000002000000000010660000000100002' +
+            '000000094fc82d29efd361cc32d19a3dd4437de50b24014e30e9b82a64e4762931a5234000000000e8000000002000020000000404d00ec9f75e052ffc627e4' +
+            '4a3369b4d1569de20a63ce74e65cac9d3e19514c20000000faca3a33c92d77eb6d8abd923b9079baf840bbb89df48928ab61a3cfbe43574d40000000d78b0ee' +
+            '4f30363bbce8c2bede340e53e909226df35da40cec6e50a29f063eb18e349db1c7e7cac8561ae4bec87d7c8ce9a8c60b2e5be9bbf83c67551a9587bed'
+            Set-StoreConfig -Name 'Secret' -Value ($secret | ConvertTo-SecureString -Force)
             $secret = Get-StoreConfig -Name 'Secret' -AsPlainText
             $secret | Should -Be 'Something'
         }
