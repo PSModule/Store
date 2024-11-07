14c82f2c03d5e2278c9ee3d4b2506c365fb04749
Marius Storhaug <marstor@hotmail.com>
2024-11-07 18:26:58 +0100

    🩹 [Patch]: Add some additional logging (#29)
    ## Description
    
    - Add some additional logging
    
    ## Type of change
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [ ] 📖 [Docs]
    - [x] 🪲 [Fix]
    - [ ] 🩹 [Patch]
    - [ ] ⚠️ [Security fix]
    - [ ] 🚀 [Feature]
    - [ ] 🌟 [Breaking change]
    
    ## Checklist
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas

--- src/functions/private/Initialize-VariableStore.ps1
+++ src/functions/private/Initialize-VariableStore.ps1
@@ -11,9 +11,12 @@
     )
 
     $folderName = ".$($Name -replace '^\.')".ToLower()
+    Write-Verbose "Variable store folder:      [$folderName]"
     $configFilePath = Join-Path -Path $HOME -ChildPath "$folderName/config.json"
-
-    if (-not (Test-Path -Path $configFilePath)) {
+    Write-Verbose "Variable store file:        [$configFilePath]"
+    $configFileExists = Test-Path -Path $configFilePath
+    Write-Verbose "Variable store file exists: [$configFileExists]"
+    if (-not $configFileExists) {
         $null = New-Item -Path $configFilePath -ItemType File -Force
         Set-StoreVariable -Name 'ConfigFilePath' -Value $configFilePath
         Set-StoreVariable -Name 'Name' -Value $Name
