c209d3a26238ab89c45c8a1ed91839377b9041c4
Marius Storhaug <marstor@hotmail.com>
2024-11-11 19:07:00 +0100

    Enhance Get-Store to support wildcard name matching and add tab completion

--- src/functions/public/Store/Get-Store.ps1
+++ src/functions/public/Store/Get-Store.ps1
@@ -10,27 +10,73 @@
         Get-Store -Name 'MySecret'
 
         Get the store called 'MySecret' from the vault.
+
+        .EXAMPLE
+        Get-Store -Name 'My*'
+
+        Get all stores that match the pattern 'My*' from the vault.
+
+        .EXAMPLE
+        Get-Store
+
+        Get all stores from the vault.
     #>
     [OutputType([hashtable])]
     param (
         # The name of the secret vault.
-        [Parameter(Mandatory)]
+        [Parameter()]
         [string] $Name,
 
         # Set everything as plain text.
         [Parameter()]
         [switch] $AsPlainText
     )
+
     $secretVault = Get-SecretVault | Where-Object { $_.Name -eq $script:Config.SecretVaultName }
     if (-not $secretVault) {
         return $null
     }
-    $secretInfo = Get-SecretInfo -Vault $secretVault.Name | Where-Object { $_.Name -eq $Name }
-    if (-not $secretInfo) {
+
+    $secretInfos = Get-SecretInfo -Vault $secretVault.Name
+    if (-not $secretInfos) {
         return $null
     }
-    $metadata = $secretInfo | Select-Object -ExpandProperty Metadata
-    $metadata + @{
-        Secret = Get-Secret -Name $Name -Vault $script:Config.SecretVaultName -AsPlainText:$AsPlainText
+
+    if ($Name) {
+        if ($Name -like '*') {
+            $secretInfos = $secretInfos | Where-Object { $_.Name -like $Name }
+        } else {
+            $secretInfos = $secretInfos | Where-Object { $_.Name -eq $Name }
+        }
+    }
+
+    $stores = @()
+    foreach ($secretInfo in $secretInfos) {
+        $metadata = $secretInfo | Select-Object -ExpandProperty Metadata
+        $store = $metadata + @{
+            Secret = Get-Secret -Name $secretInfo.Name -Vault $script:Config.SecretVaultName -AsPlainText:$AsPlainText
+        }
+        $stores += $store
+    }
+
+    return $stores
+}
+
+# Register tab completer for the Name parameter
+Register-ArgumentCompleter -CommandName Get-Store -ParameterName Name -ScriptBlock {
+    param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
+
+    $secretVault = Get-SecretVault | Where-Object { $_.Name -eq $script:Config.SecretVaultName }
+    if (-not $secretVault) {
+        return
+    }
+
+    $secretInfos = Get-SecretInfo -Vault $secretVault.Name
+    if (-not $secretInfos) {
+        return
+    }
+
+    $secretInfos | Where-Object { $_.Name -like "$wordToComplete*" } | ForEach-Object {
+        [System.Management.Automation.CompletionResult]::new($_.Name, $_.Name, 'ParameterValue', $_.Name)
     }
 }
