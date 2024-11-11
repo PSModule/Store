9881140420faa9259c1347a362f23d3bacbe670e
Marius Storhaug <marstor@hotmail.com>
2024-11-11 21:59:48 +0100

    Fix param name usage on rename-Context

--- src/functions/private/Rename-Context.ps1
+++ src/functions/private/Rename-Context.ps1
@@ -4,13 +4,13 @@
         Renames a secret in the specified vault.
 
         .DESCRIPTION
-        The `Rename-Secret` function renames a secret in the specified vault.
+        The `Rename-Context` function renames a secret in the specified vault.
         The function retrieves the existing secret's value and metadata (tags) and creates a new secret with the new name.
         If the new secret is created successfully, the old secret is removed.
         If the new secret is not created successfully the new secret is cleaned up.
 
         .EXAMPLE
-        Rename-Secret -Name 'OldSecret' -NewName 'NewSecret' -VaultName 'MyVault'
+        Rename-Context -Name 'OldSecret' -NewName 'NewSecret' -VaultName 'MyVault'
 
         .NOTES
         General notes
@@ -33,27 +33,27 @@
 
     # Retrieve the existing secret's value and metadata (tags)
     try {
-        $secretValue = Get-Secret -Name $OldSecretName -Vault $VaultName
-        $secretInfo = Get-SecretInfo -Name $OldSecretName -Vault $VaultName
+        $secretValue = Get-Secret -Name $Name -Vault $VaultName
+        $secretInfo = Get-SecretInfo -Name $Name -Vault $VaultName
 
         # Check if the old secret exists
         if (-not $secretInfo) {
-            Write-Verbose "Secret '$OldSecretName' not found in vault '$VaultName'."
+            Write-Verbose "Secret '$Name' not found in vault '$VaultName'."
             return
         }
 
         # Create a new secret with the new name, copying the value and tags
-        Set-Secret -Name $NewSecretName -Secret $secretValue -Vault $VaultName
-        Write-Verbose "New secret '$NewSecretName' created in vault '$VaultName'."
+        Set-Secret -Name $NewName -Secret $secretValue -Vault $VaultName
+        Write-Verbose "New secret '$NewName' created in vault '$VaultName'."
 
         # Verify that the new secret exists
-        $newSecretInfo = Get-SecretInfo -Name $NewSecretName -Vault $VaultName
+        $newSecretInfo = Get-SecretInfo -Name $NewName -Vault $VaultName
         if ($newSecretInfo) {
             # Remove the old secret
-            Remove-Secret -Name $OldSecretName -Vault $VaultName
-            Write-Verbose "Old secret '$OldSecretName' removed from vault '$VaultName'."
+            Remove-Secret -Name $Name -Vault $VaultName
+            Write-Verbose "Old secret '$Name' removed from vault '$VaultName'."
         } else {
-            Write-Verbose "Failed to create new secret '$NewSecretName'. Old secret was not removed."
+            Write-Verbose "Failed to create new secret '$NewName'. Old secret was not removed."
         }
     } catch {
         Write-Error "Error occurred: $_"
