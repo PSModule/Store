168a633db002ab40394cb46876f1f8d7746e6e49
Marius Storhaug <marstor@hotmail.com>
2024-11-11 19:21:54 +0100

    Update `Remove-Store` to support wildcard name matching and improve error handling

--- src/functions/public/Store/Remove-Store.ps1
+++ src/functions/public/Store/Remove-Store.ps1
@@ -1,4 +1,4 @@
-﻿function Remove-Store {
+﻿filter Remove-Store {
     <#
         .SYNOPSIS
         Remove a store from the vault.
@@ -10,17 +10,38 @@
         Remove-Store -Name 'MySecret'
 
         Removes the store called 'MySecret' from the vault.
+
+        .EXAMPLE
+        'MySecret*' | Remove-Store
+
+        Removes all stores matching the pattern 'MySecret*' from the vault.
     #>
     [OutputType([void])]
     [CmdletBinding(SupportsShouldProcess)]
     param (
         # The name of the secret vault.
-        [Parameter(Mandatory)]
+        [Parameter(
+            Mandatory,
+            ValueFromPipeline
+        )]
         [string] $Name
     )
-    if ($PSCmdlet.ShouldProcess('Remove-Secret', $Name)) {
-        Get-SecretInfo | Where-Object { $_.Name -eq $Name } | ForEach-Object {
-            Remove-Secret -Name $_.Name -Vault $script:Config.SecretVaultName
+
+    $secretVault = Get-SecretVault | Where-Object { $_.Name -eq $script:Config.SecretVaultName }
+    if (-not $secretVault) {
+        Write-Error 'Secret vault not found.'
+        return
+    }
+
+    $secretInfos = Get-SecretInfo -Vault $secretVault.Name | Where-Object { $_.Name -like $Name }
+    if (-not $secretInfos) {
+        Write-Error 'No matching stores found.'
+        return
+    }
+
+    foreach ($secretInfo in $secretInfos) {
+        if ($PSCmdlet.ShouldProcess('Remove-Secret', $secretInfo.Name)) {
+            Remove-Secret -Name $secretInfo.Name -Vault $script:Config.SecretVaultName
         }
     }
 }
