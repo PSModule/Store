34f2a522ec55130da7c44bafcff68699c1534cc7
Marius Storhaug <marstor@hotmail.com>
2024-11-11 20:06:13 +0100

    Enhance Get-StoreConfig function to support wildcard patterns and improve verbose logging

--- src/functions/public/StoreConfig/Get-StoreConfig.ps1
+++ src/functions/public/StoreConfig/Get-StoreConfig.ps1
@@ -3,15 +3,21 @@
 function Get-StoreConfig {
     <#
         .SYNOPSIS
-        Get a named value from the store.
+        Retrieve a named value from the store.
 
         .DESCRIPTION
-        Get a named value from the store.
+        This function retrieves a named value from the specified store.
+        If the value is a secret, it can be returned as plain text using the -AsPlainText switch.
 
         .EXAMPLE
         Get-StoreConfig -Name 'ApiBaseUri' -Store 'GitHub'
 
         Get the value of 'ApiBaseUri' config from the GitHub store.
+
+        .EXAMPLE
+        Get-StoreConfig -Name 'Api*' -Store 'GitHub'
+
+        Get all configuration values from the GitHub store that match the wildcard pattern 'Api*'.
     #>
     [OutputType([object])]
     [CmdletBinding()]
@@ -25,9 +31,26 @@ function Get-StoreConfig {
         [switch] $AsPlainText,
 
         # The store to get the configuration from.
-        [Parameter()]
+        [Parameter(Mandatory)]
         [string] $Store
     )
 
-    (Get-Store -Name $Store -AsPlainText:$AsPLainText).$Name
+    Write-Verbose "Getting store configuration for store: [$Store]"
+    $storeConfig = Get-Store -Name $Store -AsPlainText:$AsPlainText
+
+    if ($null -eq $storeConfig) {
+        Write-Verbose "No configuration found for store: [$Store]"
+        return
+    }
+
+    Write-Verbose "Filtering configuration properties with name like: [$Name]"
+    $matchingProperties = $storeConfig.PSObject.Properties.Where({ $_.Name -like $Name })
+
+    if ($matchingProperties.Count -eq 0) {
+        Write-Verbose "No matching properties found for name: [$Name]"
+        return
+    }
+
+    Write-Verbose "Found matching properties: [$($matchingProperties.Name -join ', ')]"
+    $matchingProperties.Value
 }
