abb95bea2fdbc8d1b114407a0c94c0a7c793f401
Marius Storhaug <marstor@hotmail.com>
2024-11-12 01:58:58 +0100

    Fix

--- src/variables/private/Config.ps1
+++ src/variables/private/Config.ps1
@@ -1,5 +1,5 @@
-﻿$script:Config = @{
-    Context = @{
+﻿$script:Config = [pscustomobject]@{
+    Context = [pscustomobject]@{
         Name             = 'PSModule.Store'                   # $script:Config.Context.Name
         ContextVaultName = 'SecretStore'                      # $script:Config.Context.VaultName
         ContextVaultType = 'Microsoft.PowerShell.SecretStore' # $script:Config.Context.VaultType
