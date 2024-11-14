1b46279f243a4fc38f412aaafdb9abf1f7751647
Marius Storhaug <marstor@hotmail.com>
2024-11-14 22:49:44 +0100

    Refactor context initialization in loader to simplify parameter assignment

--- src/loader.ps1
+++ src/loader.ps1
@@ -1,12 +1,10 @@
 ﻿### This is the backend configuration for the functionality
 try {
     $initContextParams = @{
-        Name = (Get-ContextSetting -Name VaultName -Context $script:Config.Name) ?? $script:Config.Context.VaultName
-        Type = (Get-ContextSetting -Name VaultType -Context $script:Config.Name) ?? $script:Config.Context.VaultType
+        Name = $script:Config.Context.VaultName
+        Type = $script:Config.Context.VaultType
     }
-    $vault = Initialize-ContextVault @initContextParams
-    $script:Config.Context.VaultName = $vault.Name
-    $script:Config.Context.VaultType = $vault.ModuleName
+    Initialize-ContextVault @initContextParams
 } catch {
     Write-Error $_
     throw "Failed to initialize secret vault"
