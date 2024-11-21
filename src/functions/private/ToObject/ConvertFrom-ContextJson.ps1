b8428b78d210c15cea92af1e3ac411e35d1cb96c
Marius Storhaug <marstor@hotmail.com>
2024-11-21 16:08:52 +0100

    🩹 [Refactor]: Migrate JSON conversion functions to new module structure and enhance documentation

--- src/functions/private/ToObject/ConvertFrom-ContextJson.ps1
+++
@@ -1,9 +0,0 @@
-﻿function ConvertFrom-ContextJson {
-    param (
-        [Parameter(Mandatory)]
-        [string] $JsonString
-    )
-
-    $hashtableObject = $JsonString | ConvertFrom-Json -Depth 100 -AsHashtable
-    return Convert-ContextHashtableToObjectRecursive $hashtableObject
-}
