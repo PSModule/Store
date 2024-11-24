9c61966c97765679be7251d1697147bedbf328cd
Marius Storhaug <marstor@hotmail.com>
2024-11-24 20:19:51 +0100

    Fix conversion to get secrets

---
+++ src/functions/private/Get-ContextInfo.ps1
@@ -0,0 +1,36 @@
+﻿function Get-ContextInfo {
+    <#
+        .SYNOPSIS
+        Retrieves all context info from the context vault.
+
+        .DESCRIPTION
+        Retrieves all context info from the context vault.
+
+        .EXAMPLE
+        Get-ContextInfo
+
+        Get all context info from the context vault.
+    #>
+    param()
+
+    $vaultName = $script:Config.VaultName
+    $secretPrefix = $script:Config.SecretPrefix
+
+    Write-Verbose "Retrieving all context info from [$vaultName]"
+
+    Get-SecretInfo -Vault $vaultName | ForEach-Object {
+        $name64 = $_.Name
+        if (Test-Base64 -Base64String $name64) {
+            $name = ConvertFrom-Base64 -Base64String $name64
+            if ($name.StartsWith($secretPrefix)) {
+                Write-Verbose " + $name ($name64)"
+                [pscustomobject]@{
+                    Name64   = $name64
+                    Name     = $name
+                    Metadata = $_.Metadata
+                    Type     = $_.Type
+                }
+            }
+        }
+    }
+}
--- src/functions/public/Context/Get-Context.ps1
+++ src/functions/public/Context/Get-Context.ps1
@@ -37,15 +37,7 @@ filter Get-Context {
         $secretPrefix = $script:Config.SecretPrefix
         $fullID = "$secretPrefix$ID"
 
-        $contextNames = Get-SecretInfo -Vault $vaultName | Select-Object -ExpandProperty Name | ForEach-Object {
-            if (Test-Base64 -Base64String $_) {
-                $name = ConvertFrom-Base64 -Base64String $_
-                if ($name.StartsWith($secretPrefix)) {
-                    Write-Verbose " + $name"
-                    $name
-                }
-            }
-        }
+        $contextInfos = Get-ContextInfo
     }
 
     process {
@@ -56,17 +48,16 @@ filter Get-Context {
                 Write-Verbose "Return 0 contexts from [$vaultName]"
                 return
             } elseif ($ID.Contains('*')) {
-                Write-Verbose "Retrieving contexts matching [$ID] from [$vaultName]"
-                $contexts = $contextNames | Where-Object { $_ -like $fullID }
+                Write-Verbose "Retrieving contexts like [$ID] from [$vaultName]"
+                $contextInfos = $contextInfos | Where-Object { $_.Name -like $fullID }
             } else {
                 Write-Verbose "Retrieving context [$ID] from [$vaultName]"
-                $contexts = $contextNames | Where-Object { $_ -eq $fullID }
+                $contextInfos = $contextInfos | Where-Object { $_.Name -eq $fullID }
             }
 
-            Write-Verbose "Found [$($contexts.Count)] contexts in [$vaultName]"
-            $contexts | ForEach-Object {
-                Write-Verbose " - $($_.Name)"
-                $contextJson = $_ | Get-Secret -AsPlainText
+            Write-Verbose "Found [$($contextInfos.Count)] contexts in [$vaultName]"
+            $contextInfos | ForEach-Object {
+                $contextJson = Get-Secret -Name $_.Name64 -Vault $vaultName -AsPlainText
                 ConvertFrom-ContextJson -JsonString $contextJson
             }
         } catch {
@@ -84,10 +75,9 @@ Register-ArgumentCompleter -CommandName Get-Context -ParameterName ID -ScriptBlo
     param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
     $null = $commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter
 
-    Get-SecretInfo -Vault $vaultName |
-        Where-Object { (ConvertFrom-Base64 -Base64String $_.Name) -like "$($script:Config.SecretPrefix)$wordToComplete*" } |
+    Get-ContextInfo | Where-Object { $_.Name -like "$($script:Config.SecretPrefix)$wordToComplete*" } |
         ForEach-Object {
-            $Name = (ConvertFrom-Base64 -Base64String $_.Name) -replace "^$($script:Config.SecretPrefix)"
+            $Name = $_.Name -replace "^$($script:Config.SecretPrefix)"
             [System.Management.Automation.CompletionResult]::new($Name, $Name, 'ParameterValue', $Name)
         }
 }
