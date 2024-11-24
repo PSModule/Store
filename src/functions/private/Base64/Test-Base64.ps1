7c3328f6609750ffad739fb6c3d6f16f328d4f87
Marius Storhaug <marstor@hotmail.com>
2024-11-24 19:43:40 +0100

    🆕 [Add]: Implement Test-Base64 function to validate Base64 strings

---
+++ src/functions/private/Base64/Test-Base64.ps1
@@ -0,0 +1,26 @@
+﻿function Test-Base64 {
+    <#
+        .SYNOPSIS
+        Test if a string is a valid Base64 string.
+
+        .DESCRIPTION
+        Test if a string is a valid Base64 string.
+
+        .EXAMPLE
+        Test-Base64 -Base64String 'U29tZSBkYXRh'
+        True
+
+        Returns $true as the string is a valid Base64 string.
+    #>
+    [OutputType([bool])]
+    [CmdletBinding()]
+    param (
+        [string] $Base64String
+    )
+    try {
+        [Convert]::FromBase64String($Base64String) | Out-Null
+        return $true
+    } catch {
+        return $false
+    }
+}
