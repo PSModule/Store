9efb54f0aed3e93f47dd28eebef9ac0903120431
Marius Storhaug <marstor@hotmail.com>
2024-12-07 19:32:19 +0100

    test

--- tests/Context.Tests.ps1
+++ tests/Context.Tests.ps1
@@ -6,12 +6,13 @@ param()
 
 BeforeAll {
     Get-SecretInfo | Remove-Secret
+    $VerbosePreference = 'Continue'
 }
 
 Describe 'Context' {
     Context 'Function: Set-Context' {
         It "Set-Context -ID 'TestID1'" {
-            { Set-Context -ID 'TestID1' -Verbose } | Should -Not -Throw
+            { Set-Context -ID 'TestID1'} | Should -Not -Throw
 
             $result = Get-Context -ID 'TestID1'
             $result | Should -Not -BeNullOrEmpty
