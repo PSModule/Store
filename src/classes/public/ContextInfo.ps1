95c918d6a169de07176aaadfad4946e369951683
Marius Storhaug <marstor@hotmail.com>
2024-12-08 11:45:24 +0100

    Add `ContextInfo` class

---
+++ src/classes/public/ContextInfo.ps1
@@ -0,0 +1,19 @@
+﻿class ContextInfo {
+    [string] $ID
+    [hashtable] $Metadata
+    [string] $SecretName
+    [string] $SecretType
+    [string] $VaultName
+
+    ContextInfo([hashtable]$Properties) {
+        foreach ($Property in $Properties.Keys) {
+            $this.$Property = $Properties.$Property
+        }
+    }
+
+    ContextInfo([PSCustomObject]$Object) {
+        $Object.PSObject.Properties | ForEach-Object {
+            $this.($_.Name) = $_.Value
+        }
+    }
+}
