edac0ebea219916af1fa936f07eb109b08062a88
Marius Storhaug <marstor@hotmail.com>
2024-12-09 21:14:32 +0100

    🩹 [Patch]: Enhance verbose output for secret management in tests

--- tests/Context.Tests.ps1
+++ tests/Context.Tests.ps1
@@ -5,8 +5,14 @@
 param()
 
 BeforeAll {
-    Get-SecretInfo- Verbose | Remove-Secret -Verbose
-    Get-SecretVault -Verbose | Unregister-SecretVault -Verbose
+    $secrets = Get-SecretInfo -Verbose
+    Write-Verbose "Secrets: $($secrets.Count)" -Verbose
+    Write-Verbose ($secrets | Format-Table | Out-String) -Verbose
+    $secrets | Remove-Secret -Verbose
+    $vault = Get-SecretVault -Verbose
+    Write-Verbose "Vault: $($vault.Count)" -Verbose
+    Write-Verbose ($vault | Format-Table | Out-String) -Verbose
+    $vault | Unregister-SecretVault -Verbose
 }
 
 Describe 'Functions' {
