141b4d21dd8d1937dc8447eac388236046ee81d1
Marius Storhaug <marstor@hotmail.com>
2024-12-19 10:27:13 +0100

    Update Get-ContextInfo to support wildcard ID parameter for flexible context retrieval

--- src/functions/public/Get-ContextInfo.ps1
+++ src/functions/public/Get-ContextInfo.ps1
@@ -15,13 +15,19 @@
         Get-ContextInfo -ID 'my-context'
 
         Get the context info for the context with the ID 'my-context'.
+
+        .EXAMPLE
+        Get-ContextInfo -ID 'my-*'
+
+        Get all context info for contexts with IDs starting with 'my-'.
     #>
     [OutputType([ContextInfo])]
     [CmdletBinding()]
     param(
         # The name of the context to retrieve from the vault.
         [Parameter()]
-        [string] $ID
+        [SupportsWildcards()]
+        [string] $ID = '*'
     )
 
     begin {
@@ -35,7 +41,7 @@
     process {
         Write-Debug "Retrieving all context info from [$vaultName]"
 
-        Get-SecretInfo -Vault $vaultName -Verbose:$false -Name "$secretPrefix$ID*" | ForEach-Object {
+        Get-SecretInfo -Vault $vaultName -Verbose:$false -Name "$secretPrefix$ID" | ForEach-Object {
             [ContextInfo]@{
                 ID         = ($_.Name -replace "^$secretPrefix")
                 Metadata   = $_.Metadata + @{}
